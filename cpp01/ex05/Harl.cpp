fb649c52f43e55c97dc26ff80e39195bdeb4fc98
Chloezhu010 <chloe.zhu.x@gmail.com>
2025-05-16 16:52:55 +0200

    updated harl.cpp

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,22 +6,35 @@
 /*   By: czhu <czhu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 13:59:14 by czhu              #+#    #+#             */
-/*   Updated: 2025/05/16 16:11:41 by czhu             ###   ########.fr       */
+/*   Updated: 2025/05/16 16:19:15 by czhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-void    debug(void)
+/* simple output functions */
+void    Harl::debug(void)
 {
     std::cout << "[DEBUG]: I love having extra bacon for my double cheese burger!" << std::endl;
 }
 
-void    info(void)
+void    Harl::info(void)
+{
+    std::cout << "[INFO]: I cannot believe adding extra bacon costs more money" << std::endl;
+}
+
+void    Harl::warning(void)
+{
+    std::cout << "[WARNING]: I think I deserve to have some etra bacon for free" << std::endl;
+}
+
+void    Harl::error(void)
+{
+    std::cout << "[ERROR]: This is unacceptable!" << std::endl;
+}
+
+/* ptr to output functions based on the level */
+void    Harl::complain(std::string level)
 {
     
 }
-
-void    warning(void)
-
-void    error(void);
