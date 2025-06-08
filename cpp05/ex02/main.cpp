01292bda6da5ea1cb5b8f21e295ea9ed64562aa3
chloezhu010 <chloe.zhu.x@gmail.com>
2025-06-08 21:46:59 +0200

    updated ex02

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chloe <chloe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 15:26:28 by chloe             #+#    #+#             */
-/*   Updated: 2025/06/08 21:26:23 by chloe            ###   ########.fr       */
+/*   Updated: 2025/06/08 21:44:09 by chloe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,33 +20,33 @@ int main()
 {
     std::cout << "----Constructors----\n";
     Bureaucrat A("Alice", 1);
-    ShrubberyCreationForm form("home");
+    ShrubberyCreationForm shrubbery("shrub");
     RobotomyRequestForm   robot("robo");
     PresidentialPardonForm pardon("pardo");    
 
-    // std::cout << "\n----Test ShrubberyCreationForm----\n";
-    // std::cout << "----Basic info----\n";
-    // std::cout << form << std::endl;
-    // std::cout << A << std::endl;
-    // std::cout << "----Test execute()----\n";
-    // /* attempt to sign the form */
-    // try{
-    //     form.beSign(A);
-    // }
-    // catch (std::exception& e)
-    // {
-    //     std::cout << e.what();
-    // }
-    // /* attempt to execute the form */
-    // try{
-    //     form.execute(A);
-    // }
-    // catch (std::exception& e)
-    // {
-    //     std::cout << e.what();
-    // }
-    // std::cout << "----Test executeForm()----\n";
-    // A.executeForm(form);
+    std::cout << "\n----Test ShrubberyCreationForm----\n";
+    std::cout << "----Basic info----\n";
+    std::cout << shrubbery << std::endl;
+    std::cout << A << std::endl;
+    std::cout << "----Test execute()----\n";
+    /* attempt to sign the form */
+    try{
+        shrubbery.beSign(A);
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what();
+    }
+    /* attempt to execute the form */
+    try{
+        shrubbery.execute(A);
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what();
+    }
+    std::cout << "----Test executeForm()----\n";
+    A.executeForm(shrubbery);
 
     std::cout << "\n----Test RobotomyRequestForm----\n";
     std::cout << "----Basic info----\n";
